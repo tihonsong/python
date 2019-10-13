bf3a6b8366574f1d08faef0fc445fdfc7de86499
tihon <t.song@kebrus.ru>
2019-10-13 13:07:16 +0300

    practice_6, add dict in practice_3

--- lesson02/practice_3.py
+++ lesson02/practice_3.py
@@ -6,9 +6,9 @@ list_time_of_year = ["Зима", "Весна", "Лето", "Осень"]
 list_group_month = [(12, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11)]
 month = 0
 
-while not ((1<=month) and (month <= 12)):
+while not ((1 <= month) and (month <= 12)):
     month = int(input("Вводите месяцу (01 - 12): "))
-    if (1<=month) and (month <= 12):
+    if (1 <= month) and (month <= 12):
         continue
     print("Неправильная цифира!")
 
@@ -19,9 +19,10 @@ for idx, grp in enumerate(list_group_month):
 
 print(time_of_year)
 
-dict_time_of_year = zip(list_time_of_year, list_group_month)
+# dict_time_of_year = zip(list_time_of_year, list_group_month)
+dict_time_of_year = {"Зима": (12, 1, 2), "Весна": (3, 4, 5), "Лето": (6, 7, 8), "Осень": (9, 10, 11)}
 
-for key, grp in dict_time_of_year:
+for key, grp in dict_time_of_year.items():
     if month in grp:
         time_of_year = key
         break
