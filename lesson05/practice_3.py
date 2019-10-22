5753da073ae1878f4e8bbfe42b3cb07bce4607a2
tihon <t.song@kebrus.ru>
2019-10-23 00:14:36 +0300

    practice 3,4

--- lesson05/practice_3.py
+++ lesson05/practice_3.py
@@ -4,6 +4,24 @@
    дохода сотрудников.
 """
 try:
+    with open("practice_3.txt") as file_obj:
+        count_worker = 0
+        sum_salary = 0
+        while True:
+            file_line = file_obj.readline()
+            if file_line == '':
+                print("-" * 100)
+                print(f"Средный зарплатат : {sum_salary / count_worker:.02f}")
+                break
+
+            name, salary = file_line.split(":")
+            salary = int(salary)
+            if salary < 20000:
+                count_worker += 1
+                sum_salary += salary
+                print(f"{name}: {salary}")
 
 except FileNotFoundError:
-    print("Данный файл не существует")
+    print("Данный файл не существует!")
+except ValueError:
+    print("Заплата нужно число!")
